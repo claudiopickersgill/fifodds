7dad5e203dcec7b17115b9f02e1c1dd3af0f9f20
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 17:27:29 -0300

    Update add_jogo.py

--- pages/jogos/add_jogo.py
+++ pages/jogos/add_jogo.py
@@ -9,14 +9,14 @@ def add_jogo():
         st.write("DataFrame Atual:")
         st.write(df)
 
-    # Adicionando novas linhas
-    st.header("Adicionar Novas Linhas:")
-    new_rows = st.text_area("Digite as novas linhas no formato CSV:", "")
-    new_rows = pd.read_csv(pd.compat.StringIO(new_rows)) if new_rows else pd.DataFrame()
-    
-    if st.button("Adicionar Linhas"):
-        df = add_rows(df, new_rows)
-        st.write("DataFrame Atualizado:")
-        st.write(df)
-        df.to_csv(file_path, index=False)
-        st.success("Linhas adicionadas com sucesso!")
+# Adicionando novas linhas
+st.header("Adicionar Novas Linhas:")
+new_rows = st.text_area("Digite as novas linhas no formato CSV:", "")
+new_rows = pd.read_csv(pd.compat.StringIO(new_rows)) if new_rows else pd.DataFrame()
+
+if st.button("Adicionar Linhas"):
+    df = add_rows(df, new_rows)
+    st.write("DataFrame Atualizado:")
+    st.write(df)
+    df.to_csv(file_path, index=False)
+    st.success("Linhas adicionadas com sucesso!")
