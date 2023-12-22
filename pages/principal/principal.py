dd05d46e087e51dd517280207d7e0bed04d72608
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 17:02:32 -0300

    Update principal.py

--- pages/principal/principal.py
+++ pages/principal/principal.py
@@ -5,4 +5,4 @@ def principal():
     st.title('Alooo, Fifih!!!')
     st.write('Usa o Menu do lado pra navegar entre as diferentes fases do projeto')
     st.write('Vou deixar de brinde essa imagem que encontrei nos confins do meu HD')
-    st.image('data/img/DSC05254.JPG')
+    st.image('data/img/odds.png')
