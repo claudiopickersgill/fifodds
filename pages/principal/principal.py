918b3bb70cf99af091f0314961d0e6819e072aa5
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 17:33:36 -0300

    Update principal.py

--- pages/principal/principal.py
+++ pages/principal/principal.py
@@ -5,7 +5,6 @@ from pages.jogos import cria_csv
 def principal():
     st.title('Alooo, Fifih!!!')
     st.write('Usa o Menu do lado pra navegar entre as diferentes fases do projeto')
-    st.write('Vou deixar de brinde essa imagem que encontrei nos confins do meu HD')
     st.image('data/img/odds.png')
 
     file_path = "data/cvs/Jogos.csv"
