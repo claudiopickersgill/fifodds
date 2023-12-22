06f732600cf2d3855479fe00eb377b3d2ceb0e9a
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 17:19:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ st.set_page_config(layout="wide")
 # Menu lateral
 st.sidebar.title("Menu")
 menu = st.sidebar.selectbox('Selecione uma Página', [
-    'Principal', 'Calcular ODDS'])
+    'Principal', 'Calcular ODDS', 'Incluir Jogo na Base'])
 
 if menu == 'Principal':
     principal.principal()
 elif menu == 'Calcular ODDS':
-    odds.odds()
+    odds.odds()   
