d043b364bdd2329df021552a8719693675af03be
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 20:18:56 -0300

    Update pagina_teste.py

--- pages/pagina_teste/pagina_teste.py
+++ pages/pagina_teste/pagina_teste.py
@@ -1,15 +1,12 @@
 import streamlit as st
-
-# streamlit_app.py
-
-import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+def teste():
+    # Create a connection object.
+    conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+    df = conn.read()
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+    # Print results.
+    for row in df.itertuples():
+        st.write(f"{row.name} has a :{row.pet}:")
