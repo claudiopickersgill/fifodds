08b28846b788048f86c46ddddbe923da992e09d7
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 17:18:13 -0300

    Update principal.py

--- pages/principal/principal.py
+++ pages/principal/principal.py
@@ -6,7 +6,7 @@ def principal():
     st.title('Alooo, Fifih!!!')
     st.write('Usa o Menu do lado pra navegar entre as diferentes fases do projeto')
     st.write('Vou deixar de brinde essa imagem que encontrei nos confins do meu HD')
-    st.image('data/img/DSC05254.JPG')
+    st.image('data/img/odds.png')
 
     file_path = "data/cvs/Jogos.csv"
     if st.button("Recarregar CSV"):
