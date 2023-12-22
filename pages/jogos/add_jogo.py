05b72670c237aacf004a723a23976081eb2a1d47
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 17:25:03 -0300

    Update add_jogo.py

--- pages/jogos/add_jogo.py
+++ pages/jogos/add_jogo.py
@@ -2,4 +2,21 @@ import streamlit as st
 import pandas as pd
 
 def add_jogo():
-  return 0
+  # Carregando o CSV existente
+    file_path = "data/cvs/Jogos.csv"
+    if st.button("Recarregar CSV"):
+        df = read_csv(file_path)
+        st.write("DataFrame Atual:")
+        st.write(df)
+
+    # Adicionando novas linhas
+    st.header("Adicionar Novas Linhas:")
+    new_rows = st.text_area("Digite as novas linhas no formato CSV:", "")
+    new_rows = pd.read_csv(pd.compat.StringIO(new_rows)) if new_rows else pd.DataFrame()
+    
+    if st.button("Adicionar Linhas"):
+        df = add_rows(df, new_rows)
+        st.write("DataFrame Atualizado:")
+        st.write(df)
+        df.to_csv(file_path, index=False)
+        st.success("Linhas adicionadas com sucesso!")
