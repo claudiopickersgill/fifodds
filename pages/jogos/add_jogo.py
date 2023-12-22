289e59faad199a7637dfcb96e230b08e8b149bd2
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 18:23:02 -0300

    Update add_jogo.py

--- pages/jogos/add_jogo.py
+++ pages/jogos/add_jogo.py
@@ -26,7 +26,7 @@ def add_jogo():
     odd_equivalente = st.number_input('Digite a odd_equivalente',key='odd_equivalente')
     green = st.number_input('Digite a green',key='green')
 
-    nova_linha = pd.DataFrame({'odd': [odd], 'responsabilidade': [responsabilidade], 'lucro': [lucro], 'porcentagem': [porcentagem], 'odd_equivalente': [odd_equivalente], 'green': [green]})
+    nova_linha = pd.DataFrame({'odd': odd, 'responsabilidade': responsabilidade, 'lucro': lucro, 'porcentagem': porcentagem, 'odd_equivalente': odd_equivalente, 'green': green})
     st.write(nova_linha)
 
     if st.button("Adicionar Linhas"):
