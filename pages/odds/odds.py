de86e73e6bd40ab6d9a8d461606fc46fdad2ba01
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 14:01:34 -0300

    Update odds.py

--- pages/odds/odds.py
+++ pages/odds/odds.py
@@ -4,14 +4,16 @@ def odds():
     st.title('Aloouuu... vamos calcular a porcetagem em cima da Odd da aposta!!')
     st.write(
         'Digite o valor da Odd que deseja calcular a porcentagem:')
-    odd = st.number_input('Digite seu número:')
-    st.write(
-        'Vamos calcular o percentual com base em um lucro de R$100,00')
-    lucro = 100
+    odd = st.number_input('Digite a ODD:')
+    st.write(f'Você está calculando a porcentagem da Odd: {odd}"
+    st.write('\n')
     responsabilidade = (odd - 1) * lucro
-    st.write('Para ter um lucro de R$100,00...')
-    st.write('Precisaremos apostar (responsabilidade) um total de:',round(responsabilidade))
-    st.write('O valor da Responsabilidade está arredondado!!!')
-    st.write('Agora, vamos ver a porcetagem de lucro, baseado no valor investido (responsabilidade), lucro, e Odds:')
+    st.write('Precisaremos apostar (responsabilidade) um total de: ',round(responsabilidade))
+    st.write('OBS: O valor da Responsabilidade está arredondado!!!')
+    lucro = 100
+    st.write('Para obter um lucro de R${lucro}')
+    st.write('\n')
+    st.write('Vamos calcular o percentual com base em um lucro de R$100,00')    
+    st.write(f'Agora, vamos ver a porcetagem de lucro, baseado no na responsabilidade de {responsabilidade}, no lucro de {lucro}, e na Odd de {odd}:')
     porcentagem = (lucro * 100) / responsabilidade
     st.write(f'De acordo com os dados disponíveis, temos uma porcentagem de lucro igual a: {porcentagem} %')
