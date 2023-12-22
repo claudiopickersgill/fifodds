d4b22054fcbe377df4298864b5778c706cd639e0
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 17:21:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from pages.principal import principal
 from pages.odds import odds
+from pages.jogos import add_jogo
 
 st.set_page_config(layout="wide")
 
@@ -12,4 +13,6 @@ menu = st.sidebar.selectbox('Selecione uma Página', [
 if menu == 'Principal':
     principal.principal()
 elif menu == 'Calcular ODDS':
-    odds.odds()   
+    odds.odds()
+elif manu == 'Incluir Jogo na Base':
+    add_jogo.add_jogo()
