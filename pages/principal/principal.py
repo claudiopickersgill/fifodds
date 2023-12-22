233fbf2dc1e220b19c205947d64faae7ca6d34a5
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 17:05:42 -0300

    Update principal.py

--- pages/principal/principal.py
+++ pages/principal/principal.py
@@ -4,5 +4,4 @@ import streamlit as st
 def principal():
     st.title('Alooo, Fifih!!!')
     st.write('Usa o Menu do lado pra navegar entre as diferentes fases do projeto')
-    st.write('Vou deixar de brinde essa imagem que encontrei nos confins do meu HD')
     st.image('data/img/odds.png')
