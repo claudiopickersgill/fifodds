b263a29266cdea41767586ceb1bcb3e7aee7f0c8
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 17:34:22 -0300

    Update add_jogo.py

--- pages/jogos/add_jogo.py
+++ pages/jogos/add_jogo.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 from pages.jogos import cria_csv
 
+def add_rows(df, new_rows):
+    df = pd.concat([df, new_rows], ignore_index=True)
+    return df
+
 def add_jogo():
     file_path = "data/cvs/Jogos.csv"
     if st.button("Carregar CSV"):
@@ -9,14 +13,14 @@ def add_jogo():
         st.write("DataFrame Atual:")
         st.write(df)
 
-# # Adicionando novas linhas
-# st.header("Adicionar Novas Linhas:")
-# new_rows = st.text_area("Digite as novas linhas no formato CSV:", "")
-# new_rows = pd.read_csv(pd.compat.StringIO(new_rows)) if new_rows else pd.DataFrame()
+    # Adicionando novas linhas
+    st.header("Adicionar Novas Linhas:")
+    new_rows = st.text_area("Digite as novas linhas no formato CSV:", "")
+    new_rows = pd.read_csv(pd.compat.StringIO(new_rows)) if new_rows else pd.DataFrame()
 
-# if st.button("Adicionar Linhas"):
-#     df = add_rows(df, new_rows)
-#     st.write("DataFrame Atualizado:")
-#     st.write(df)
-#     df.to_csv(file_path, index=False)
-#     st.success("Linhas adicionadas com sucesso!")
+    if st.button("Adicionar Linhas"):
+        df = add_rows(df, new_rows)
+        st.write("DataFrame Atualizado:")
+        st.write(df)
+        df.to_csv(file_path, index=False)
+        st.success("Linhas adicionadas com sucesso!")
