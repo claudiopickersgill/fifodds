6f816ff2f5e28aa7cf41fada67a0a743a5fb8590
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 20:31:27 -0300

    Update pagina_teste.py

--- pages/pagina_teste/pagina_teste.py
+++ pages/pagina_teste/pagina_teste.py
@@ -1,12 +1,20 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+from google.oauth2 import service_account
+import gspread
+import pandas as pd
 
-def teste():
+def teste():   
+
     # Create a connection object.
-    conn = st.connection("gsheets", type=GSheetsConnection)
-
-    df = conn.read()
-
-    # Print results.
-    for row in df.itertuples():
-        st.write(f"{row.name} has a :{row.pet}:")
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+        ],
+    )
+    gc = gspread.authorize(credentials)
+    
+    # Get the Google Sheet by URL.
+    sheet_url = st.secrets["private_gsheets_url"]
+    sheet = gc.open_by_url(sheet_url)
