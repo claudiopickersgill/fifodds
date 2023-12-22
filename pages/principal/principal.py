5f2a0df0d6cef23bd5a98d10998cf2c280cb5cc2
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 17:17:24 -0300

    Add files via upload

--- pages/principal/principal.py
+++ pages/principal/principal.py
@@ -1,7 +1,15 @@
 import streamlit as st
+from pages.jogos import cria_csv
 
 
 def principal():
     st.title('Alooo, Fifih!!!')
     st.write('Usa o Menu do lado pra navegar entre as diferentes fases do projeto')
-    st.image('data/img/odds.png')
+    st.write('Vou deixar de brinde essa imagem que encontrei nos confins do meu HD')
+    st.image('data/img/DSC05254.JPG')
+
+    file_path = "data/cvs/Jogos.csv"
+    if st.button("Recarregar CSV"):
+        df = cria_csv.read_csv(file_path)
+        st.write("DataFrame Atual:")
+        st.write(df)
