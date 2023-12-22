7d03cad5ec47ba108f14ebb6e31d2cc2d96ac477
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 14:31:50 -0300

    Update principal.py

--- pages/principal/principal.py
+++ pages/principal/principal.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 
 def principal():
-    st.title('Dashboard de Análise de Expectativa de Vida')
-    st.write(
-        'Navegue a partir do menu lateral para exibir as diferentes fases do projeto')
+    st.title('Alooo, Fifih!!!')
+    st.write('Usa o Menu do lado pra navegar entre as diferentes fases do projeto')
+    st.write('Vou deixar de brinde essa imagem que encontrei nos confins do meu HD')
+    st.image('https://github.com/claudiopickersgill/fifodds/blob/30e8347cf61eaac06b108ddf954babe8481e45ff/data/img/DSC05254.JPG')
