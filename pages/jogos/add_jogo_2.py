60754bbd2571ea58f74cbab80671d4e4f2cdebad
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-23 01:07:33 -0300

    Create add_jogo_2.py

---
+++ pages/jogos/add_jogo_2.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+odd = st.number_input('Digite a Odd', key='odd')
+responsabilidade = st.number_input('Digite a responsabilidade',key='responsabilidade')
+lucro = st.number_input('Digite a lucro',key='lucro')
+porcentagem = st.number_input('Digite a porcentagem',key='porcentagem')
+odd_equivalente = st.number_input('Digite a odd_equivalente',key='odd_equivalente')
+green = st.number_input('Digite a green',key='green')
+
+nova_linha = pd.DataFrame({'odd': [odd], 'responsabilidade': [responsabilidade], 'lucro': [lucro], 'porcentagem': [porcentagem], 'odd_equivalente': [odd_equivalente], 'green': [green]})
+
+if st.button("Adicionar Linhas"):
+  # Call the function to insert data into the Google Sheet
+  insert_data_into_sheet(nova_linha)
+        # df_novo = pd.concat([df, nova_linha], ignore_index=True)
+        # st.write("DataFrame Atualizado:")
+        # st.write(df_novo)
+        # df_novo.to_csv(file_path, index=False)
+  st.success("Linhas adicionadas com sucesso!")
