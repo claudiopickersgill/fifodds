b0d956ed1c68820a999a3ff93fc3d3028b20060c
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-22 21:28:35 -0300

    Update pagina_teste.py

--- pages/pagina_teste/pagina_teste.py
+++ pages/pagina_teste/pagina_teste.py
@@ -4,10 +4,10 @@ from google.oauth2 import service_account
 import gspread
 import pandas as pd
 
-@st.cache_data
-def load_data(sheets_url):
-    csv_url = sheets_url.replace('edit?usp=sharing', 'export?format=csv&gid=0')
-    return pd.read_csv(csv_url)
+# @st.cache_data
+# def load_data(sheets_url):
+#     csv_url = sheets_url.replace('edit?usp=sharing', 'export?format=csv&gid=0')
+#     return pd.read_csv(csv_url)
 
 def teste():   
 
@@ -21,7 +21,7 @@ def teste():
     gc = gspread.authorize(credentials)
     
     # Get the Google Sheet by URL.
-    sheet_url = load_data(st.secrets["private_gsheets_url"])
+    sheet_url = pd.read_csv(st.secrets["private_gsheets_url"])
     # sheet = gc.open_by_url(sheet_url)
     df = pd.DataFrame(sheet_url)
     st.write(sheet)
