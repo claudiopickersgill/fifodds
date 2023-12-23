9ebe30890aa481ffddc3a7f26236d37f8a0e181a
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2023-12-23 00:51:27 -0300

    Update add_jogo.py

--- pages/jogos/add_jogo.py
+++ pages/jogos/add_jogo.py
@@ -6,20 +6,21 @@ from pages.pagina_teste import pagina_teste
 # file_path = "data/cvs/Jogos.csv"
 
 def add_jogo():
-  df = pagina_teste.teste()
-  st.write(df)
+  worksheet = pagina_teste.teste()
+  st.write(worksheet)
 
-  last_row = df.shape[0]
-  st.write(last_row)
+  # last_row = df.shape[0]
+  # st.write(last_row)
 #   # Function to find the last filled row in the worksheet.
 # def find_last_filled_row(worksheet):
 #     return len(worksheet.get_all_values()) + 1
-  values = df.values.tolist()
-  st.write(values)
+  # values = df.values.tolist()
+  # st.write(values)
 # # Function to insert data into the Google Sheet after the last filled row.
 # def insert_data_into_sheet(dataframe):
 #     worksheet = sheet.get_worksheet(0)  # Replace 0 with the index of your desired worksheet
 #     values = dataframe.values.tolist()
+  
 
 #     # Find the last filled row
 #     last_filled_row = find_last_filled_row(worksheet)
@@ -44,19 +45,18 @@ def add_jogo():
 #         st.write("DataFrame Atual:")
 #         st.write(df)
 
-#     odd = st.number_input('Digite a Odd', key='odd')
-#     responsabilidade = st.number_input('Digite a responsabilidade',key='responsabilidade')
-#     lucro = st.number_input('Digite a lucro',key='lucro')
-#     porcentagem = st.number_input('Digite a porcentagem',key='porcentagem')
-#     odd_equivalente = st.number_input('Digite a odd_equivalente',key='odd_equivalente')
-#     green = st.number_input('Digite a green',key='green')
+    # odd = st.number_input('Digite a Odd', key='odd')
+    # responsabilidade = st.number_input('Digite a responsabilidade',key='responsabilidade')
+    # lucro = st.number_input('Digite a lucro',key='lucro')
+    # porcentagem = st.number_input('Digite a porcentagem',key='porcentagem')
+    # odd_equivalente = st.number_input('Digite a odd_equivalente',key='odd_equivalente')
+    # green = st.number_input('Digite a green',key='green')
 
-#     nova_linha = pd.DataFrame({'odd': odd, 'responsabilidade': responsabilidade, 'lucro': lucro, 'porcentagem': porcentagem, 'odd_equivalente': odd_equivalente, 'green': green})
-#     st.write(nova_linha)
+    # nova_linha = pd.DataFrame({'odd': [odd], 'responsabilidade': [responsabilidade], 'lucro': [lucro], 'porcentagem': [porcentagem], 'odd_equivalente': [odd_equivalente], 'green': [green]})
 
-#     if st.button("Adicionar Linhas"):
-#         df_novo = pd.concat([df, nova_linha], ignore_index=True)
-#         st.write("DataFrame Atualizado:")
-#         st.write(df_novo)
-#         df_novo.to_csv(file_path, index=False)
-#         st.success("Linhas adicionadas com sucesso!")
+    # if st.button("Adicionar Linhas"):
+    #     df_novo = pd.concat([df, nova_linha], ignore_index=True)
+    #     st.write("DataFrame Atualizado:")
+    #     st.write(df_novo)
+    #     df_novo.to_csv(file_path, index=False)
+    #     st.success("Linhas adicionadas com sucesso!")
